ffda0d9eb607f79ddb95cc9eacdc5b3f7653e33d
Morihiro Nagano <morihiro-nagano@jslabo.com>
2018-12-06 18:03:14 +0900

    result1

--- shindan.js
+++ shindan.js
@@ -10,82 +10,82 @@ Shindan.Questions.push({
     "text":"今よりもっとわたしを活かす道がある気がしている",
     "target":"もやもや期"
 });
-// Shindan.Questions.push({
-//     "text":"頑張っている人を見て、つい自分と比べてしまう",
-//     "target":"もやもや期"
-// });
-// Shindan.Questions.push({
-//     "text":"自分の強みは？と聞かれるのが苦手",
-//     "target":"もやもや期"
-// });
-// Shindan.Questions.push({
-//     "text":"SNSに仕事のことをアップするのは、友人の目が気になる",
-//     "target":"ふわふわ期"
-// });
-// Shindan.Questions.push({
-//     "text":"自分よりすごい人がいるのに、私なんかでいいんだろうか？と不安になる",
-//     "target":"ふわふわ期"
-// });
-// Shindan.Questions.push({
-//     "text":"収入はアップしたいが、単価を上げるのは不安がある",
-//     "target":"ふわふわ期"
-// });
-// Shindan.Questions.push({
-//     "text":"自分の気持ちを言っているつもりで、すぐに不安になってしまう",
-//     "target":"ふわふわ期"
-// });
-// Shindan.Questions.push({
-//     "text":"存在を知って欲しいとは思うが、アピールするのは苦手だ",
-//     "target":"ふわふわ期"
-// });
-// Shindan.Questions.push({
-//     "text":"あなたの仕事は？と聞かれて短く一言では表現できない",
-//     "target":"ぐるぐる期"
-// });
-// Shindan.Questions.push({
-//     "text":"次に取得したい資格、学びたい事が次々生まれる。",
-//     "target":"ぐるぐる期"
-// });
-// Shindan.Questions.push({
-//     "text":"人から言われるアドバイスがピンとこない",
-//     "target":"ぐるぐる期"
-// });
-// Shindan.Questions.push({
-//     "text":"複数のスキルがあり、いったい自分の何が人から求められるだろうと考えてしまう",
-//     "target":"ぐるぐる期"
-// });
-// Shindan.Questions.push({
-//     "text":"パーティーや交流会など華やかな場では気後れする",
-//     "target":"こつこつ期"
-// });
-// Shindan.Questions.push({
-//     "text":"2年後も今の仕事で、より充実感を持てるようになることが目標だ",
-//     "target":"こつこつ期"
-// });
-// Shindan.Questions.push({
-//     "text":"目標はあるが、人に伝えるのは恥ずかしい気がする。",
-//     "target":"こつこつ期"
-// });
-// Shindan.Questions.push({
-//     "text":"自撮りやインスタ映え写真をアップする事はあまり共感できない",
-//     "target":"こつこつ期"
-// });
-// Shindan.Questions.push({
-//     "text":"人から認められることが喜びにつながる",
-//     "target":"がんがん期"
-// });
-// Shindan.Questions.push({
-//     "text":"自分の直感を信じて行動できる",
-//     "target":"がんがん期"
-// });
-// Shindan.Questions.push({
-//     "text":"考えてばかりいるよりも、行動をすることが大事だと思う",
-//     "target":"がんがん期"
-// });
-// Shindan.Questions.push({
-//     "text":"今よりもステージアップするためには、行動の質と量を増やす事が大切だ。",
-//     "target":"がんがん期"
-// });
+Shindan.Questions.push({
+    "text":"頑張っている人を見て、つい自分と比べてしまう",
+    "target":"もやもや期"
+});
+Shindan.Questions.push({
+    "text":"自分の強みは？と聞かれるのが苦手",
+    "target":"もやもや期"
+});
+Shindan.Questions.push({
+    "text":"SNSに仕事のことをアップするのは、友人の目が気になる",
+    "target":"ふわふわ期"
+});
+Shindan.Questions.push({
+    "text":"自分よりすごい人がいるのに、私なんかでいいんだろうか？と不安になる",
+    "target":"ふわふわ期"
+});
+Shindan.Questions.push({
+    "text":"収入はアップしたいが、単価を上げるのは不安がある",
+    "target":"ふわふわ期"
+});
+Shindan.Questions.push({
+    "text":"自分の気持ちを言っているつもりで、すぐに不安になってしまう",
+    "target":"ふわふわ期"
+});
+Shindan.Questions.push({
+    "text":"存在を知って欲しいとは思うが、アピールするのは苦手だ",
+    "target":"ふわふわ期"
+});
+Shindan.Questions.push({
+    "text":"あなたの仕事は？と聞かれて短く一言では表現できない",
+    "target":"ぐるぐる期"
+});
+Shindan.Questions.push({
+    "text":"次に取得したい資格、学びたい事が次々生まれる。",
+    "target":"ぐるぐる期"
+});
+Shindan.Questions.push({
+    "text":"人から言われるアドバイスがピンとこない",
+    "target":"ぐるぐる期"
+});
+Shindan.Questions.push({
+    "text":"複数のスキルがあり、いったい自分の何が人から求められるだろうと考えてしまう",
+    "target":"ぐるぐる期"
+});
+Shindan.Questions.push({
+    "text":"パーティーや交流会など華やかな場では気後れする",
+    "target":"こつこつ期"
+});
+Shindan.Questions.push({
+    "text":"2年後も今の仕事で、より充実感を持てるようになることが目標だ",
+    "target":"こつこつ期"
+});
+Shindan.Questions.push({
+    "text":"目標はあるが、人に伝えるのは恥ずかしい気がする。",
+    "target":"こつこつ期"
+});
+Shindan.Questions.push({
+    "text":"自撮りやインスタ映え写真をアップする事はあまり共感できない",
+    "target":"こつこつ期"
+});
+Shindan.Questions.push({
+    "text":"人から認められることが喜びにつながる",
+    "target":"がんがん期"
+});
+Shindan.Questions.push({
+    "text":"自分の直感を信じて行動できる",
+    "target":"がんがん期"
+});
+Shindan.Questions.push({
+    "text":"考えてばかりいるよりも、行動をすることが大事だと思う",
+    "target":"がんがん期"
+});
+Shindan.Questions.push({
+    "text":"今よりもステージアップするためには、行動の質と量を増やす事が大切だ。",
+    "target":"がんがん期"
+});
 Shindan.Questions.forEach(function(val){
     console.log(val.text);
     val.status = 0;
@@ -103,10 +103,11 @@ Shindan.MakeNextQuestion = function(){
     $(".slide_question_base").clone()
         .removeClass("slide_question_base")
         .addClass("slide_question").appendTo('.main_content');
-    target[0].status = Shindan.State.AnsNo;
+    var qTarget = target[Math.floor(Math.random() * target.length)];
+    qTarget.status = Shindan.State.AnsNo;
     var $quest = $(".slide_question").last();
     $quest.find("h2").text("Question " + Shindan.State.AnsNo);
-    $quest.find(".question_text").text(target[0].text);
+    $quest.find(".question_text").text(qTarget.text);
 };
 Shindan.ShowSlide = function(){
     if ($quest.hasClass('off')) {
@@ -138,24 +139,70 @@ Shindan.Init = function(){
         Shindan.MakeNextQuestion();
         Shindan.ShowSlide($(this).closest('.slide_question'));//hide slide
         if(Shindan.State.AnsNo > Shindan.Questions.length){
-            var circleSize = $(window).width();
-            if (circleSize < $(window).height()){
-                circleSize = $(window).height();
-            }
-            $(".result").animate({
-                width: circleSize,
-                height: circleSize
-            }, 3000 ,'easeIn')
-            .queue(function () {
-                $(this).css('border-radius', '0');
-                $(this).find('.slide_inner').show();
-                $(this).dequeue();
-            });
+            Shindan.ShowResult();
             return;
         }
         $quest = $('.slide_question').last();//show slide
         Shindan.ShowSlide($quest);
     });
+};
+Shindan.ShowResult = function(){
+    var resultType = Shindan.Questions
+        .filter(function(val){return val.result;})
+        .map(function(val){return val.target;})
+        .mode() || "もやもや期";
+    $(".result_type").text(resultType);
+    var circleSize = $(window).width();
+    if (circleSize < $(window).height()){
+        circleSize = $(window).height();
+    }
+    $(".result").animate({
+        width: circleSize,
+        height: circleSize
+    }, 1000 ,'swing')
+    .animate({'border-radius':'0%'},300)
+    .queue(function () {
+        $(this).css('width', '100vw');
+        $(this).css('height', '100vh');
+        $(this).find('.slide_inner').show();
+        $(this).dequeue();
+    });
+
+};
+
+Array.prototype.mode = function () {
+    if (this.length === 0){
+        //配列の個数が0だとエラーを返す。
+        //throw new Error("配列の長さが0のため最頻値が計算できません");
+        //nullを返しても困らない時(配列の中にnullが無い時)はnullを返すように実装しても良い。
+        return null;
+    }
+    //回数を記録する連想配列
+    var counter = {};
+    //本来の値を入れた辞書
+    var nativeValues = {};
+
+    //最頻値とその出現回数を挿入する変数
+    var maxCounter = 0;
+    var maxValue = null;
+
+    for (var i = 0; i < this.length; i++) {
+        //counterに存在しなければ作る。keyは型を区別する
+        if (!counter[this[i] + "_" + typeof this[i]]) {
+            counter[this[i] + "_" + typeof this[i]] = 0;
+        }
+        counter[this[i] + "_" + typeof this[i]]++;
+        nativeValues[this[i] + "_" + typeof this[i]] = this[i];
+
+    }
+    for (var j = 0; j < Object.keys(counter).length; j++) {
+        key = Object.keys(counter)[j];
+        if (counter[key] > maxCounter) {
+            maxCounter = counter[key];
+            maxValue = nativeValues[key];
+        }
+    }
+    return maxValue;
 };
 $(function () {
     Shindan.Init();
